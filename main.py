66d241ef9013d2b3418e46d571897ce747ba7e33
VictorEmanuel4y <120692285+VictorEmanuel4y@users.noreply.github.com>
2022-12-15 21:26:23 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("""Victor
+       15/12/2022
+       I am Signing up for Replit´s 100 days of Python  
+      challenge!
+      I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day
+      I will be using Replit, an amazing online IDE so i can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+      😎😎😎😎😎😎😎😎😎😎😎😎😎😎
+      You can follow my progress at https://replit.com/@ManuelGonsalves""")
