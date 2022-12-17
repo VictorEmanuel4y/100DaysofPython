ec0ad104a8898d464a7ec390609618ccde6ea010
VictorEmanuel4y <120692285+VictorEmanuel4y@users.noreply.github.com>
2022-12-17 16:10:16 -0300

    Add files via upload

---
+++ Concat.py
@@ -0,0 +1,7 @@
+Food = input("Name a food: ")
+Plant = input("Name a type of plant: ")
+Method = input("Name a method of cooking: ")
+Word = input("What word describes burned food? :")
+DIY = input("Name a DIV item: ")
+
+print("Menu","\n",Method,Food,"with",Word,Plant,"on a bed of",DIY)
