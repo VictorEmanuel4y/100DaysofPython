7f26a1d4aa44665eb9cd1b19806a5958c3c35b02
VictorEmanuel4y <120692285+VictorEmanuel4y@users.noreply.github.com>
2022-12-18 23:49:25 -0300

    Add files via upload

---
+++ PrintColor.py
@@ -0,0 +1,12 @@
+print("===Your adventure Simulator===")
+print("""You will be asked a bunch of questions 
+          then we will make you up a amazing 
+          story with You as the star! 🤩""")
+print()
+name = input("\033[30m"+"Your name: ")
+enemy = input("\033[31m"+"your enemy: ")
+superpower = input("\033[32m"+"Your superpower: ")
+
+print("""Your story begins when the impostor tried to kill 
+ you""","\033[30m",name)
+print("He was a sussy baka when saw the vent open, but","\033[31m",enemy,"didint know that the impostos was an impostor, so then was a innocent, he deliver the impostor and you became famous and won superpower named","\033[32m",superpower )
