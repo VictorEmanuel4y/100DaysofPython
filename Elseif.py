1666ec13544ae9b603e5d1f9319e86178cadc92c
VictorEmanuel4y <120692285+VictorEmanuel4y@users.noreply.github.com>
2022-12-21 16:39:03 -0300

    Add files via upload

---
+++ Elseif.py
@@ -0,0 +1,13 @@
+login = input("Login: ")
+senha = input("Senha: ")
+print(login , "\n", senha)
+if(login == "victor" and senha== "123") :
+  print("Hi victor")
+elif(login == "santa" and senha == "123"):
+  print("Hi santa")
+
+else: 
+ print("invalid user")
+  
+
+  
