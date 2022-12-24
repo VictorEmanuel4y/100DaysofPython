6a8634ab2ff8278ba973b103acbd250b20ccdb5a
VictorEmanuel4y <120692285+VictorEmanuel4y@users.noreply.github.com>
2022-12-24 15:32:02 -0300

    Add files via upload

---
+++ Ifinsideif.py
@@ -0,0 +1,7 @@
+name = input("Say your name: ")
+if(name == "victor"):
+  midname = input("Say your midname: ")
+  if(midname == "emanuel"):
+    print("correct!")
+else: 
+  print("Who are you?")    
